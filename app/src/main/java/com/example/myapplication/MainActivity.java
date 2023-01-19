b9b17db667df65351e7bdc7ec67e820831aa31c7
sgrebocki <szymon.grebocki@gmail.com>
2023-01-19 01:09:01 +0100

    Login functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +16,48 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnCreateAcc;
 
+    private String Username = "admin";
+    private String Password = "admin";
+
+    boolean isValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreateAcc = findViewById(R.id.btnCreateAcc);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputUsername = etUsername.getText().toString();
+                String inputPassword = etPassword.getText().toString();
+
+                if(inputUsername.isEmpty() || inputPassword.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Proszę wpisać nazwę użytkownika oraz hasło", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    isValid = validate(inputUsername, inputPassword);
+
+                    if(!isValid){
+                        Toast.makeText(MainActivity.this, "Błędny login lub hasło!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Cześć "+ inputUsername + "!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean validate(String username, String password){
+        if (username.equals(Username) && password.equals(Password)){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
